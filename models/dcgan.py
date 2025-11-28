9e124fe52f449a40e524f97a20518bd97393c9c9
sebargith <131008747+sebargith@users.noreply.github.com>
2025-11-27 23:01:42 -0300

    Implementación modelo DCGAN

---
+++ models/dcgan.py
@@ -0,0 +1,69 @@
+# models/dcgan.py
+import torch
+import torch.nn as nn
+
+# Basado en DCGAN típico para 64x64
+
+class GeneratorDCGAN(nn.Module):
+    def __init__(self, z_dim=100, ngf=64, nc=3):
+        super().__init__()
+        self.main = nn.Sequential(
+            # Entrada: Z -> (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(z_dim, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+
+            # (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+
+            # (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+
+            # (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+
+            # nc x 64 x 64
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh(),
+        )
+
+    def forward(self, z):
+        return self.main(z)
+
+
+class DiscriminatorDCGAN(nn.Module):
+    def __init__(self, ndf=64, nc=3):
+        super().__init__()
+        self.main = nn.Sequential(
+            # nc x 64 x 64 -> (ndf) x 32 x 32
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # (ndf) x 32 x 32 -> (ndf*2) x 16 x 16
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # (ndf*2) x 16 x 16 -> (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # (ndf*4) x 8 x 8 -> (ndf*8) x 4 x 4
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            # (ndf*8) x 4 x 4 -> 1
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+        )
+
+    def forward(self, x):
+        out = self.main(x).view(-1, 1).squeeze(1)
+        return out
