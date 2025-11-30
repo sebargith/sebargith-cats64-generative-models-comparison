4ff570b9bfd3c8be47c70f4acd66c8d8aedc5328
sebargith <131008747+sebargith@users.noreply.github.com>
2025-11-29 22:05:20 -0300

    Generador de samples

---
+++ training/generate_dcgan_samples.py
@@ -0,0 +1,14 @@
+# training/generate_dcgan_samples.py
+from pathlib import Path
+
+import torch
+from torchvision.utils import save_image
+
+from models.dcgan import GeneratorDCGAN
+
+
+def main():
+
+    
+if __name__ == "__main__":
+    main()
