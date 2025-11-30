31a0be9a29dad9ff2e4f879bebe83c59f117c8e9
sebargith <131008747+sebargith@users.noreply.github.com>
2025-11-29 22:05:50 -0300

    Update generador de samples

--- training/generate_dcgan_samples.py
+++ training/generate_dcgan_samples.py
@@ -8,7 +8,34 @@ from models.dcgan import GeneratorDCGAN
 
 
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Device:", device)
 
-    
+    z_dim = 100
+    num_samples = 64  # para un grid, luego puedes generar miles
+
+    # Cargar el generador entrenado
+    netG = GeneratorDCGAN(z_dim=z_dim).to(device)
+    state_dict = torch.load("outputs/models/dcgan_G.pth", map_location=device)
+    netG.load_state_dict(state_dict)
+    netG.eval()
+
+    out_dir = Path("outputs/samples")
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    with torch.no_grad():
+        z = torch.randn(num_samples, z_dim, 1, 1, device=device)
+        fake = netG(z)
+        fake = (fake + 1) / 2  # de [-1,1] a [0,1]
+
+        save_image(
+            fake,
+            out_dir / "dcgan_final_grid.png",
+            nrow=8,
+        )
+
+    print("Guardado grid en outputs/samples/dcgan_final_grid.png")
+
+
 if __name__ == "__main__":
     main()
